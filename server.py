509a3a3ae0f318865f4fb06be739800f81f42579
Adamsky <napora.adam@gmail.com>
2019-05-10 21:05:13 +0100

    Added object detection API call

--- server.py
+++ server.py
@@ -6,54 +6,35 @@ from imagezmq import imagezmq
 import argparse
 import imutils
 import cv2
+import requests
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-    help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-    help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.2,
-    help="minimum probability to filter weak detections")
 ap.add_argument("-mW", "--montageW", required=True, type=int,
     help="montage frame width")
 ap.add_argument("-mH", "--montageH", required=True, type=int,
     help="montage frame height")
 args = vars(ap.parse_args())
 
+# define API host/port
+PREDICT_API_URL = 'http://localhost:8000/api/predict'
+SAVE_DETECTIONS_API_URL = 'http://localhost:8000/api/detections'
+
 # initialize the ImageHub object
-imageHub = imagezmq.ImageHub()
- 
-# initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-    "sofa", "train", "tvmonitor"]
- 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+imageHub = imagezmq.ImageHub(open_port='tcp://*:5555')
 
 # initialize the consider set (class labels we care about and want
 # to count), the object count dictionary, and the frame  dictionary
-CONSIDER = set(["dog", "person", "car", "cat"])
-objCount = {obj: 0 for obj in CONSIDER}
+CONSIDER = set(["dog", "person", "car", "cat", "bird", "bicycle", "motorbike"])
+prev_obj_count, cur_obj_count  = {obj: 0 for obj in CONSIDER}, {obj: 0 for obj in CONSIDER}
 frameDict = {}
+last_active = {}
+last_check = datetime.now()
+CHANGE_DETECTION_INTERVAL_SECONDS = 5
 
-# initialize the dictionary which will contain  information regarding
-# when a device was last active, then store the last time the check
-# was made was now
-lastActive = {}
-lastActiveCheck = datetime.now()
- 
-# stores the estimated number of Pis, active checking period, and
-# calculates the duration seconds to wait before making a check to
-# see if a device was active
-ESTIMATED_NUM_PIS = 1
-ACTIVE_CHECK_PERIOD = 10
-ACTIVE_CHECK_SECONDS = ESTIMATED_NUM_PIS * ACTIVE_CHECK_PERIOD
- 
+# define colors for the bounding boxes
+COLORS = [tuple(255 * np.random.rand(3)) for i in range(5)]
+
 # assign montage width and height so we can view all incoming frames
 # in a single "dashboard"
 mW = args["montageW"]
@@ -65,101 +46,79 @@ print("[INFO] detecting: {}...".format(", ".join(obj for obj in
 while True:
     # receive RPi name and frame from the RPi and acknowledge
     # the receipt
-    (rpiName, frame) = imageHub.recv_image()
+    (rpi_name, frame) = imageHub.recv_image()
     imageHub.send_reply(b'OK')
- 
+
     # if a device is not in the last active dictionary then it means
     # that its a newly connected device
-    if rpiName not in lastActive.keys():
-        print("[INFO] receiving data from {}...".format(rpiName))
+    if rpi_name not in last_active.keys():
+        print("[INFO] device started sending data: {}...".format(rpi_name))
  
     # record the last active time for the device from which we just
     # received a frame
-    lastActive[rpiName] = datetime.now()
+    last_active[rpi_name] = datetime.now()
 
     # resize the frame to have a maximum width of 400 pixels, then
     # grab the frame dimensions and construct a blob
-    frame = imutils.resize(frame, width=400)
+    #frame = imutils.resize(frame, width=400)
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-        0.007843, (300, 300), 127.5)
- 
-    # pass the blob through the network and obtain the detections and
-    # predictions
-    net.setInput(blob)
-    detections = net.forward()
- 
-    # reset the object count for each object in the CONSIDER set
-    objCount = {obj: 0 for obj in CONSIDER}
 
-    # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
- 
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
-        if confidence > args["confidence"]:
-            # extract the index of the class label from the
-            # detections
-            idx = int(detections[0, 0, i, 1])
- 
-            # check to see if the predicted class is in the set of
-            # classes that need to be considered
-            if CLASSES[idx] in CONSIDER:
-                # increment the count of the particular object
-                # detected in the frame
-                objCount[CLASSES[idx]] += 1
- 
-                # compute the (x, y)-coordinates of the bounding box
-                # for the object
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
- 
-                # draw the bounding box around the detected object on
-                # the frame
-                cv2.rectangle(frame, (startX, startY), (endX, endY),
-                    (255, 0, 0), 2)
+    # query API for the predictions and draw boxes if 
+    # objects have been found
+    preds = []
+    try:
+        r = requests.post(PREDICT_API_URL, json={"frame": frame.tolist()})
+        resp_json = r.json()
+        preds = resp_json['results']
+    except:
+        print("[ERROR] Unable to retrieve predictions from url: {}".format(PREDICT_API_URL))
+
+    # draw boxes if predictions have been returned
+    # do it only for the tracked objects
+    cur_obj_count = {obj: 0 for obj in CONSIDER}
+    if len(preds):
+        for color, result in zip(COLORS, preds):
+            start_y = result['topleft']['y']
+            tl = (result['topleft']['x'], start_y - 15 if start_y - 15 > 15 else start_y + 15)
+            br = (result['bottomright']['x'], result['bottomright']['y'])
+            label = result['label']
+            if label in CONSIDER:
+                frame = cv2.rectangle(frame, tl, br, color, 2)
+                frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255,0), 2)
+                cur_obj_count[label] += 1
+
+    # check if change in detections occurred every few seconds
+    if (datetime.now() - last_check).seconds > CHANGE_DETECTION_INTERVAL_SECONDS:
+        last_check = datetime.now()
+        if cur_obj_count != prev_obj_count:
+            # extract detected objects
+            obj_detected = [o[0] for o in cur_obj_count.items() if o[1] > 0] 
+            try:
+                requests.post(SAVE_DETECTIONS_API_URL, json={"frame": frame.tolist(), "obj_detected": obj_detected})
+            except:
+                print("[ERROR] Unable to save detections at url: {}".format(SAVE_DETECTIONS_API_URL))
+
+            prev_obj_count = cur_obj_count
+            
+    # draw the object count on the frame
+    label = ", ".join("{}: {}".format(obj, count) for (obj, count) in cur_obj_count.items())
+    cv2.putText(frame, label, (10, h - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.358, (0, 255,0), 1)
 
     # draw the sending device name on the frame
-    cv2.putText(frame, rpiName, (10, 25),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
- 
-    # draw the object count on the frame
-    label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
-        objCount.items())
-    cv2.putText(frame, label, (10, h - 20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255,0), 2)
- 
+    cv2.putText(frame, rpi_name, (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.36, (0, 0, 255), 1)
+
     # update the new frame in the frame dictionary
-    frameDict[rpiName] = frame
+    frameDict[rpi_name] = frame
  
     # build a montage using images in the frame dictionary
     montages = build_montages(frameDict.values(), (w, h), (mW, mH))
  
     # display the montage(s) on the screen
     for (i, montage) in enumerate(montages):
-        cv2.imshow("Home pet location monitor ({})".format(i),
-            montage)
+        cv2.imshow("Home monitor ({})".format(i), montage)
  
     # detect any kepresses
     key = cv2.waitKey(1) & 0xFF
-
-    # if current time *minus* last time when the active device check
-    # was made is greater than the threshold set then do a check
-    if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
-        # loop over all previously active devices
-        for (rpiName, ts) in list(lastActive.items()):
-            # remove the RPi from the last active and frame
-            # dictionaries if the device hasn't been active recently
-            if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
-                print("[INFO] lost connection to {}".format(rpiName))
-                lastActive.pop(rpiName)
-                frameDict.pop(rpiName)
- 
-        # set the last active check time as current time
-        lastActiveCheck = datetime.now()
  
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
